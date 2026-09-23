fd314bb6c9d62a9144e4aa97cf6eac890223f27f
Daner Alejandro Salazar Colorado <daneralejandro03@gmail.com>
2026-09-23 05:07:47 -0500

    fix: corregir sintaxis YAML en pnpm-workspace.yaml e instalar dependencias de cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  adapter: cloudflare()
 });
